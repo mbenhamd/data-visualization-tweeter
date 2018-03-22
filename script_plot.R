0e69c7960e3ca343c2f0978c021171fefae8f70c
Mohamed BEN HAMDOUNE <mbenhamd@gmail.com>
2018-03-22 21:41:04 +0100

    Plots added

--- script_plot.R
+++ script_plot.R
@@ -1,4 +1,13 @@
+library(plotrix)
+getwd()
 langTabCount = read.csv("tabLangWithoutNull.csv")
 langTabCount[,2]
-pie(langTabCount[1:10,2],labels = langTabCount[1:10,1],main = "First 10 languages",sub="1 208 516 638 tweets counted")
+colnames(langTabCount)
+color = grDevices::colors()[grep('gr(a|e)y', grDevices::colors(), invert = T)]
+
+pie(langTabCount[1:10,2],explode=0.1,labels = langTabCount[1:10,1],main = "First 10 languages",col=sample(color,10),radius=0.9)
+
 sum(langTabCount[,2])
+langTabCount[1:10,1]
+
+
